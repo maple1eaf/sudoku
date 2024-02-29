21b891f437ad8ae4db484478b3a6b571ca1b4dc9
markduan <dcx5601@163.com>
2024-02-28 20:46:30 -0800

    chore: upgrade to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="sudoku",
-    version="0.1.0",
+    version="0.2.0",
     description="Sudoku",
     long_description=long_description,
     author="markduan",
